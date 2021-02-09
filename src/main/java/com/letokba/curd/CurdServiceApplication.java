5d2b5ff175f178ec47d85aba77bd0346a8376520
Wait <mrfengyong@qq.com>
2021-02-09 08:31:53 +0800

    Initial commit

---
+++ src/main/java/com/letokba/curd/CurdServiceApplication.java
@@ -0,0 +1,13 @@
+package com.letokba.curd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurdServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurdServiceApplication.class, args);
+    }
+
+}
